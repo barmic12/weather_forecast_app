58409add9393bf5a7674f47c6cec0b207c1e8924
Bartek Michalak <barmic12@gmail.com>
2019-05-31 00:47:59 +0200

    Add missing email validation for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   validates :username, presence: true, uniqueness: true
 
   validates :password, presence: true, on: :create
