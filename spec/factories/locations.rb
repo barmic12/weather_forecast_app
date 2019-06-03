00cb324f9a430d37d720478713861681c8d5ef1c
Bartek Michalak <barmic12@gmail.com>
2019-06-03 08:15:01 +0200

    Fix locations factory.

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :location do
-    api_id { Faker::Number.number }
+    api_id { Faker::Number.number(2) }
     name { Faker::Address.city }
-    country { Faker::Address.country }
+    country { Faker::Address.country_code }
     latitude { Faker::Address.latitude }
     longitude { Faker::Address.longitude }
   end
